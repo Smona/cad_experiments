9161c5b04aadb44bb95d5e8335255e90f0484e8f
Mel Bourgeois <mason.bourgeois@gmail.com>
2024-09-23 04:41:56 -0500

    Upgrade to python 3.12 & install jupyter

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
@@ -20,6 +20,7 @@
         in
         (p2n.mkPoetryEnv {
           projectDir = ./.;
+          preferWheels = true;
           overrides = p2n.defaultPoetryOverrides.extend (
             final: prev: {
               trianglesolver = prev.trianglesolver.overridePythonAttrs (old: {
@@ -33,6 +34,35 @@
               });
               yacv-server = prev.yacv-server.overridePythonAttrs (old: {
                 buildInputs = (old.buildInputs or [ ]) ++ [ prev.poetry ];
+              });
+              urllib3 = prev.urllib3.overridePythonAttrs (old: {
+                buildInputs = (old.buildInputs or [ ]) ++ [ prev.hatch-vcs ];
+              });
+              scipy = pkgs.python312Packages.scipy;
+              numpy = pkgs.python312Packages.numpy;
+              # currently broken in poetry2nix, same error as here:
+              # https://github.com/moble/quaternion/issues/155
+              numpy-quaternion = pkgs.python312Packages.quaternion;
+              casadi = pkgs.python312Packages.casadi;
+              voila = prev.voila.overridePythonAttrs (old: {
+                buildInputs = (old.buildInputs or [ ]) ++ [ prev.jupyter-packaging ];
+              });
+              # https://github.com/nix-community/poetry2nix/issues/1814
+              tinycss2 = prev.tinycss2.overridePythonAttrs (old: {
+                # remove pytest-runner
+                buildInputs = [ ];
+              });
+              # https://github.com/nix-community/poetry2nix/issues/1814
+              pillow = prev.pillow.overridePythonAttrs (old: {
+                # remove pytest-runner
+                nativeBuildInputs = [ pkgs.pkg-config ];
+              });
+              cad-viewer-widget = prev.cad-viewer-widget.overridePythonAttrs (old: {
+                buildInputs = (old.buildInputs or [ ]) ++ [
+                  prev.hatchling
+                  prev.hatch-nodejs-version
+                  prev.hatch-jupyter-builder
+                ];
               });
             }
           );
