9651e2c82b804ae4259db86587893aa756192604
Mel Bourgeois <mason.bourgeois@gmail.com>
2024-09-24 18:06:48 -0500

    pull out export_image to normal python code

---
+++ cad_experiments/utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from build123d import LineType, Part, Compound, ExportSVG, Rectangle, export_stl
+from IPython.display import publish_display_data
+
+
+def export_image(part: Part, path: str):
+    svg_path = f"renders/{path}.svg"
+    stl_path = f"meshes/{path}.stl"
+    view_port_origin = (-100, -50, 30)
+
+    visible, hidden = part.project_to_viewport(view_port_origin)
+    bbox = Compound(visible + hidden).bounding_box()
+    max_dimension = max(*bbox.size)
+    exporter = ExportSVG(
+        scale=100 / max_dimension, fill_color=(30, 30, 46), line_color=None
+    )
+    exporter.add_layer("Visible", line_color=(200, 200, 200))
+    exporter.add_layer("Hidden", line_color=(99, 99, 99), line_type=LineType.ISO_DOT)
+    exporter.add_shape(Rectangle(width=bbox.size.X * 1.2, height=bbox.size.Y * 1.2))
+    exporter.add_shape(visible, layer="Visible")
+    exporter.add_shape(hidden, layer="Hidden")
+    exporter.write(svg_path)
+
+    export_stl(part, stl_path, ascii_format=True)
+
+    message = f"""
+    [[file:{svg_path}]]      [[file:{stl_path}][STL file]]
+    """
+    publish_display_data({"text/org": message})
--- open.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-from webbrowser import open_new_tab
-
-open_new_tab("https://yeicor-3d.github.io/yet-another-cad-viewer/")
